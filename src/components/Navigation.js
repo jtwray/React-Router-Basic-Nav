01e45cc039c182aca77b14c1aaccfc0b08586e37
jtwray <tuckerwray84@gmail.com>
2019-09-03 21:56:18 -0400

    Setups a basic React-Router spa with Nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
-const Navigation = () => {
+const Navigation=() => {
 	return (
 		<div>
 			<div className='App'>
